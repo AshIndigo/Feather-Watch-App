7c7312358c1165fd08a035faf8e74162b6745239
AshIndigo <firedragonman958@gmail.com>
2017-12-20 17:24:19 +0000

    Notif adding and removing truly works now

--- app/src/main/java/com/ashindigo/watchprog/NotificationListener.java
+++ app/src/main/java/com/ashindigo/watchprog/NotificationListener.java
@@ -7,6 +7,8 @@ import android.service.notification.StatusBarNotification;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NotificationListener extends NotificationListenerService {
 
     @Nullable
@@ -17,14 +19,20 @@ public class NotificationListener extends NotificationListenerService {
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn){
-        Log.i("Notif Test Add", sbn.getPackageName());
-        BLEThread.notifs.add(sbn);
+        Log.i("Notif Test Add", sbn.getNotification().extras.getString("android.title"));
+        BLEThread.notifs.put(sbn.getNotification().extras.getString("android.title"), sbn.getNotification().extras.getString("android.title"));
+        Log.i("Notif List", BLEThread.notifs.toString());
     }
 
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn){
-        Log.i("Notif Test Remove", sbn.getPackageName());
-        BLEThread.notifs.remove(sbn);
+        Log.i("Notif Test Remove", sbn.getNotification().extras.getString("android.title"));
+        Log.i("Notif List", BLEThread.notifs.toString());
+        if (BLEGattCallback.chara != null && MainActivity.gattD != null) {
+            BLEGattCallback.chara.setValue("N|" + new ArrayList(BLEThread.notifs.keySet()).indexOf(sbn.getNotification().extras.getString("android.title")) + "|" + "null" + "|E"); // Just try notif title
+            MainActivity.gattD.writeCharacteristic(BLEGattCallback.chara);
+        }
+        BLEThread.notifs.remove(sbn.getNotification().extras.getString("android.title"));
     }
 
 }
